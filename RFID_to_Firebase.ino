ac757be8c69e015bab4aad29b9743ce591053741
Amith Tony <amithtony@gmail.com>
2020-12-07 10:34:17 +0530

    Adding NTP timestamping

--- RFID_to_Firebase.ino
+++ RFID_to_Firebase.ino
@@ -1,16 +1,22 @@
 #include <Hash.h> //using sha1 hash
 #include <SPI.h> // SPI library- Serial Peripheral Interface Protocol
 #include <MFRC522.h> // RFID library (https://github.com/miguelbalboa/rfid)
+#include <NTPClient.h>
 #include "FirebaseESP8266.h"
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 
-#define FIREBASE_HOST "****"      
-#define FIREBASE_AUTH "****"            
+#define FIREBASE_HOST "https://nodemcu-1206.firebaseio.com/"      
+#define FIREBASE_AUTH "YtEadYbnayiO7jIrytZo1z7eWaBJ3EjuQ8uUcFEk"            
 #define WIFI_SSID "Tenda_2B1C00"                                  
-#define WIFI_PASSWORD "****"
+#define WIFI_PASSWORD "Rex@mra81"
 
 #define SS_PIN D4 //Slave Select
 #define RST_PIN D2 // Reset Pin
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Set up mfrc522 on the Arduino
 FirebaseData firebaseData;
 FirebaseJson json;
@@ -31,7 +37,8 @@ void setup() {
   Serial.print("Connected");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());                               //prints local IP address
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                 // connect to the firebase
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);  // connect to the firebase
+  timeClient.begin();
  
 }
  
@@ -46,9 +53,11 @@ void loop() {
       String resultid = sha1(id);//Encrypting id using SHA1 Algorithm
       Serial.println(" " + resultid); // Print out of encrypted id is complete.
       delay(2000);
+      timeClient.update();
       json.set("chipid",resultid);
       json.set("readerid","H101");
-      if (Firebase.pushJSON(firebaseData, "/rfid/id", json)) {
+      json.set("timestamp",timeClient.getFormattedTime());
+      if (Firebase.pushJSON(firebaseData, "/rfid", json)){
 
   Serial.println(firebaseData.dataPath());
 
